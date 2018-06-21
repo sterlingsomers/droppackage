ab5d6a50ff118ca88f570ca5846bd22dffa4e92d
sterlingsomers <psomers@andrew.cmu.edu>
2018-06-20 22:02:20 -0400

    Runs a mission. should be able to update it so that it runs with different xml files

---
+++ databaseToChunks.py
@@ -0,0 +1,64 @@
+import psycopg2
+import pickle
+import numpy as np
+
+
+con = psycopg2.connect(dbname='apm_missions',user='postgres',password='sterling',host='localhost',port=32768)
+cur = con.cursor()
+filenumber = '070050'
+uuid_list = pickle.load(open(filenumber + '.p', "rb"))
+
+database = {}
+#uuid_list = [uuid_list[0]]
+for uuids in uuid_list:
+    mission_uuid = uuids['mission']
+    cur.execute("SELECT mavsim_openlog.* FROM flights JOIN mavsim_openlog ON "
+                 "flights.simulation_session_uuid = mavsim_openlog.simulation_session_uuid WHERE"
+                 " flights.uuid='{}' ORDER BY mavsim_openlog.id;".format(mission_uuid))
+
+    line = cur.fetchone()
+    previous_line = ''
+    step = {}
+    while line is not None:
+        #print(line)
+        while 'REWARD' not in line:
+
+            if "RAW_OBSERVATIONS" in line:
+                step['observation'] = line[line.index('RAW_OBSERVATIONS') + 1]
+            if "ENV_STEP_INFO" in  line:
+                step['environment'] = line[line.index('ENV_STEP_INFO') + 1]
+            if "AGENT_ACTIONS" in line:
+                step['action'] = line[line.index('AGENT_ACTIONS') + 1]
+            line = cur.fetchone()
+        if "REWARD" in line:
+            step['reward'] = line[line.index('REWARD') + 1]
+        #previous_line = line
+        if not mission_uuid in database:
+            database[mission_uuid] = []
+        database[mission_uuid].append(step.copy())
+        line = cur.fetchone()
+        #break
+
+with open(filenumber + '.dict', 'wb') as handle:
+    pickle.dump(database, handle)
+
+print("pickled the database to dictionary as {}.dict".format(filenumber))
+
+list_of_chunks_as_lists = []
+for mission_uuid in database:
+    for step in database[mission_uuid]:
+        chunk = []
+        observation = np.fromstring(step['observation'])
+        print(type(observation))
+
+
+        action = step['action']
+        environment = step['environment']
+        reward = step['reward']
+
+
+
+
+
+
+
--- run_mission.py
+++ run_mission.py
@@ -7,69 +7,70 @@ import threading
 import yaml
 import uuid
 import subprocess
+import pickle
 
-
-
-uuid = uuid.uuid4().hex
-print("uuid", uuid)
-
-msgs = [['SIM','NEW','godiland-base', uuid], ['FLIGHT', 'ARM'], ['FLIGHT', 'MS_LOAD_PAYLOAD', 0, 'food'],
-        ['FLIGHT', 'AUTO_TAKEOFF'],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],
-        ['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],
-        ['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],
-        ['FLIGHT', 'FLY_TO', 485, 29, 3],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],
-        ['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION']]
 
 def ListToFormattedString(alist):
     # Each item is right-adjusted, width=3
-    formatted_list = ["'{}'" if isinstance(i,str) else "{}" for i in alist]
-    #print(formatted_list)
-    s = '(' + ','.join(formatted_list) +')'
+    # modified from: https: // stackoverflow.com / questions / 7568627 / using - python - string - formatting - with-lists
+    formatted_list = ["'{}'" if isinstance(i, str) else "{}" for i in alist]
+    # print(formatted_list)
+    s = '(' + ','.join(formatted_list) + ')'
     return s.format(*alist)
 
-mavsim_server = ('127.0.0.1', 32786)
-send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-for msg in msgs:
-    msg = ListToFormattedString(msg)
-    print("sending", msg)
+
+uuids = []
+
+for i in range(10):
+    hiker_positions = [[70,50],[90,50],[110,50],[130,50]]
+    mission_uuid = uuid.uuid4().hex
+    session_uuid = uuid.uuid4().hex
+    instance_uuid = uuid.uuid4().hex
+    uuids.append({'mission':mission_uuid,'session':session_uuid,'instance':instance_uuid})
+    #print("uuid", auuid)
+    msgs = [['SIM', 'INSTANCE', 'instance_{}'.format(instance_uuid)],
+            ['SIM', 'SESSION', 'session_{}'.format(session_uuid)],
+            ['SIM', 'PILOT', 'ACTR_TEST'],
+            ['SIM', 'CLOSE', 'unclosed'],['SIM', 'NEW', 'godiland-base', mission_uuid], ['FLIGHT', 'ARM'], ['FLIGHT', 'MS_LOAD_PAYLOAD', 0, 'food'],
+            ['FLIGHT', 'AUTO_TAKEOFF'],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],
+            ['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],
+            ['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],
+            ['FLIGHT', 'FLY_TO', 485, 29, 3],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],
+            ['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION']]
+
+
+
+    mavsim_server = ('127.0.0.1', 32786)
+    send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    for msg in msgs:
+        msg = ListToFormattedString(msg)
+        print("sending", msg)
+        sent = send_sock.sendto(msg.encode('utf-8'),mavsim_server)
+        data, server = send_sock.recvfrom(1024)
+        print(data.decode('utf-8'))
+
+    print("take-off procedure complete.")
+
+    print("starting the q-learner.")
+
+    subprocess.run(["docker", "exec", "q-agent", "python3", "main.py", "--env-id", "apl-nav-godiland-v0", "--drop_payload_agent",
+         "--qfunction", "./q_functions/qf_v0.qf"])
+
+    print("DONE.")
+    print("reseting.")
+    msg = "('{}', '{}', '{}')".format('SIM', 'CLOSE', mission_uuid)
+    #print(sending, msg)
     sent = send_sock.sendto(msg.encode('utf-8'),mavsim_server)
     data, server = send_sock.recvfrom(1024)
     print(data.decode('utf-8'))
 
-print("take-off procedure complete.")
-subprocess.run(["docker", "exec", "q-agent", "python3", "main.py", "--env-id", "apl-nav-godiland-v0", "--drop_payload_agent",
-     "--qfunction", "./q_functions/qf_v0.qf"])
+print("compelete.")
+print(uuids)
 
-# mavsim_server = ('127.0.0.1', 32786)
-# send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# msg = '(\'SIM\', \'NEW\', \'godiland-base\', \'{}\')'.format(uuid)
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# msg = '(\'FLIGHT\', \'ARM\')'
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# msg = '(\'FLIGHT\', \'MS_LOAD_PAYLOAD\', 0, \'food\')'
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# msg = '(\'FLIGHT\',\'AUTO_TAKEOFF\')'
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# msg = '(\'FLIGHT\',\'MS_NO_ACTION\')'
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# msg = '(\'FLIGHT\', \'FLY_TO\', 485, 29, 3)'
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# msg = '(\'FLIGHT\',\'MS_NO_ACTION\')'
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
-# sent = send_sock.sendto(msg.encode('utf-8'), mavsim_server)
+with open('110050.p', 'wb') as handle:
+    pickle.dump(uuids, handle)
 
+print("uuids pickled as 110050.p")
 
