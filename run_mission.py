06d4a4966515554cf7be339fc6dd6d2a579f669a
sterlingsomers <psomers@andrew.cmu.edu>
2018-06-25 15:51:08 -0400

    An update to work by combinations

--- run_mission.py
+++ run_mission.py
@@ -27,86 +27,86 @@ hiker_positions_y = [50, 70, 90, 110]
 
 combinations = list(itertools.product(hiker_positions_x,hiker_positions_y))
 #TODO use this combination format instead.
+#sed -i 3s:.*:"  <hiker_position>190, 110</hiker_position>": /cogle/cogle-mavsim/cogle_mavsim/assets/godiland_nav_v0.xml
 
-last_hiker_x = 25
-last_hiker_y = 33
+
+
+#last_hiker_x = 25
+#last_hiker_y = 33
 #need to edit the xml file to those values: 25, 33
 
-for y in hiker_positions_y:
-    #edit the xml file...
-    sed_command = "0,/{}/s/{}/{}/".format(last_hiker_y, last_hiker_y, y)
-    subprocess.run(["docker", "exec", "q-agent", "sed", "-i", sed_command,
-                    "/cogle/cogle-mavsim/cogle_mavsim/assets/godiland_nav_v0.xml"])
-    last_hiker_y = y
-    for x in hiker_positions_x:
-
-        for i in range(1):
-
-            mission_uuid = uuid.uuid4().hex
-            session_uuid = uuid.uuid4().hex
-            instance_uuid = uuid.uuid4().hex
-            uuids.append({'mission':mission_uuid,'session':session_uuid,'instance':instance_uuid})
-            #print("uuid", auuid)
-            msgs = [['SIM', 'INSTANCE', 'instance_{}'.format(instance_uuid)],
-                    ['SIM', 'SESSION', 'session_{}'.format(session_uuid)],
-                    ['SIM', 'PILOT', 'ACTR_DATA'],
-                    ['SIM', 'CLOSE', 'unclosed'],['SIM', 'NEW', 'godiland-base', mission_uuid], ['FLIGHT', 'ARM'], ['FLIGHT', 'MS_LOAD_PAYLOAD', 0, 'food'],
-                    ['FLIGHT', 'AUTO_TAKEOFF'],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],
-                    ['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],
-                    ['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],
-                    ['FLIGHT', 'FLY_TO', 485, 29, 3],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],
-                    ['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION']]
+for combination in combinations:
+    sed_command = "3s:.*:  <hiker_position>{}, {}</hiker_position>:".format(combination[0],combination[1])
 
 
 
-            mavsim_server = ('127.0.0.1', 32786)
-            send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    for i in range(1):
 
-            for msg in msgs:
-                msg = ListToFormattedString(msg)
-                print("sending", msg)
-                sent = send_sock.sendto(msg.encode('utf-8'),mavsim_server)
-                data, server = send_sock.recvfrom(1024)
-                print(data.decode('utf-8'))
-
-            print("take-off procedure complete.")
-
-
-            #First edit the xml file
-            sed_command = "0,/{}/s/{}/{}/".format(last_hiker_x,last_hiker_x,x)
-            print(sed_command)
-            last_hiker_x = x
-            subprocess.run(["docker", "exec", "q-agent", "sed", "-i", sed_command, "/cogle/cogle-mavsim/cogle_mavsim/assets/godiland_nav_v0.xml"])
-            #Do the same for y
-
-
-            #sed - i
-            #'0,/150/s/150/170/' / cogle / cogle - mavsim / cogle_mavsim / assets / godiland_nav_v0.xml
-
-            print("starting the q-learner.")
+        mission_uuid = uuid.uuid4().hex
+        session_uuid = uuid.uuid4().hex
+        instance_uuid = uuid.uuid4().hex
+        uuids.append({'mission':mission_uuid,'session':session_uuid,'instance':instance_uuid})
+        #print("uuid", auuid)
+        msgs = [['SIM', 'INSTANCE', 'instance_{}'.format(instance_uuid)],
+                ['SIM', 'SESSION', 'session_{}'.format(session_uuid)],
+                ['SIM', 'PILOT', 'ACTR_DATA'],
+                ['SIM', 'CLOSE', 'unclosed'],['SIM', 'NEW', 'godiland-base', mission_uuid], ['FLIGHT', 'ARM'], ['FLIGHT', 'MS_LOAD_PAYLOAD', 0, 'food'],
+                ['FLIGHT', 'AUTO_TAKEOFF'],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],
+                ['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],
+                ['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],
+                ['FLIGHT', 'FLY_TO', 485, 29, 3],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],
+                ['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION'],['FLIGHT', 'MS_NO_ACTION']]
 
 
 
-            subprocess.run(["docker", "exec", "q-agent", "python3", "main.py", "--env-id", "apl-nav-godiland-v0", "--drop_payload_agent",
-                 "--qfunction", "./q_functions/qf_v0.qf"])
+        mavsim_server = ('127.0.0.1', 32786)
+        send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-            print("DONE.")
-            print("reseting.")
-            msg = "('{}', '{}', '{}')".format('SIM', 'CLOSE', mission_uuid)
-            #print(sending, msg)
+        for msg in msgs:
+            msg = ListToFormattedString(msg)
+            print("sending", msg)
             sent = send_sock.sendto(msg.encode('utf-8'),mavsim_server)
             data, server = send_sock.recvfrom(1024)
             print(data.decode('utf-8'))
 
-            print("compelete.")
-        print(uuids)
-
-        file_name = "{}-{}.p".format(x,y)
-
-        with open(file_name, 'wb') as handle:
-            pickle.dump(uuids, handle)
+        print("take-off procedure complete.")
 
 
-        print("uuids pickled as", file_name)
-        uuids = []
+        #First edit the xml file
+        #sed_command = "0,/{}/s/{}/{}/".format(last_hiker_x,last_hiker_x,x)
+        #print(sed_command)
+        #last_hiker_x = x
+        #subprocess.run(["docker", "exec", "q-agent", "sed", "-i", sed_command, "/cogle/cogle-mavsim/cogle_mavsim/assets/godiland_nav_v0.xml"])
+        #Do the same for y
+
+
+        #sed - i
+        #'0,/150/s/150/170/' / cogle / cogle - mavsim / cogle_mavsim / assets / godiland_nav_v0.xml
+
+        print("starting the q-learner.")
+
+
+
+        subprocess.run(["docker", "exec", "q-agent", "python3", "main.py", "--env-id", "apl-nav-godiland-v0", "--drop_payload_agent",
+             "--qfunction", "./q_functions/qf_v0.qf"])
+
+        print("DONE.")
+        print("reseting.")
+        msg = "('{}', '{}', '{}')".format('SIM', 'CLOSE', mission_uuid)
+        #print(sending, msg)
+        sent = send_sock.sendto(msg.encode('utf-8'),mavsim_server)
+        data, server = send_sock.recvfrom(1024)
+        print(data.decode('utf-8'))
+
+        print("compelete.")
+    print(uuids)
+
+    file_name = "{}-{}.p".format(x,y)
+
+    with open(file_name, 'wb') as handle:
+        pickle.dump(uuids, handle)
+
+
+    print("uuids pickled as", file_name)
+    uuids = []
 
